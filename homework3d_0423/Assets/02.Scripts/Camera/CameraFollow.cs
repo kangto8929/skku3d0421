d5f29554943551b153fd01ff2f31abf6dc2a7357
minju8843 <minju8843@naver.com>
2025-04-25 11:19:25 +0900

    카메라 완료

--- homework3d_0423/Assets/02.Scripts/Camera/CameraFollow.cs
+++ homework3d_0423/Assets/02.Scripts/Camera/CameraFollow.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+/*using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
@@ -9,4 +9,56 @@ public class CameraFollow : MonoBehaviour
         // interpoling, smoothing 기법이 들어갈 예쩡
         transform.position = Target.position;
     }
+}*/
+
+using Unity.Mathematics;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public CameraRotate CameraRotateScript;
+
+    public Transform FpsView;
+    public Transform TpsView;
+    public Transform QuarterView;
+
+    public float TransitionSpeed = 5f;
+
+    private Transform _currentTarget;
+
+    private void Start()
+    {
+        _currentTarget = TpsView;//초기 시점
+        CameraRotateScript.enabled = false;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            _currentTarget = FpsView;
+            CameraRotateScript.enabled = true;
+        }
+
+        else if(Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            _currentTarget = TpsView;
+            CameraRotateScript.enabled = false;
+        }
+
+        else if(Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            _currentTarget = QuarterView;
+            CameraRotateScript.enabled = false;
+        }
+
+        if(_currentTarget != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, _currentTarget.position, TransitionSpeed);
+            transform.rotation = Quaternion.Slerp(transform.rotation, _currentTarget.rotation, TransitionSpeed);
+        }
+    }
 }
+
+
+
